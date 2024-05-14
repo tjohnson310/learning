7ddfeecd02b1c1621ddfa54859851b6a057796ae
tjohnson310 <tjohnson310@users.noreply.github.com>
2024-05-14 12:10:59 -0500

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -118,7 +118,7 @@ void record_preferences(int ranks[])
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (i != j)
+            if (i < j)
             {
                 preferences[ranks[i]][ranks[j]]++;
             }

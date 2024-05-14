bc3c7c4c6d61c006a64e846da7274fe946fc3831
tjohnson310 <tjohnson310@users.noreply.github.com>
2024-05-14 12:17:49 -0500

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -133,7 +133,7 @@ void add_pairs(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = 0; j < candidate_count - 1; j++)
         {
             if (preferences[i][j] < preferences[i][j + 1])
             {

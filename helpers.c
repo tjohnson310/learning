d53bcae8235d6a039a8aeacf300be5720fbb33aa
tjohnson310 <tjohnson310@users.noreply.github.com>
2024-06-11 14:08:45 -0500

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -34,14 +34,14 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Compute sepia values
-            sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
-            sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
             sepiaBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+            sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
+            sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
 
             // Update pixel with sepia values
-            image[i][j].rgbtBlue = sepiaRed;
+            image[i][j].rgbtBlue = sepiaBlue;
             image[i][j].rgbtGreen = sepiaGreen;
-            image[i][j].rgbtRed = sepiaBlue;
+            image[i][j].rgbtRed = sepiaRed;
         }
     }
     return;

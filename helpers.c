4dcd01a1f25e3b864385b3fcea9e90cba08d9c99
tjohnson310 <tjohnson310@users.noreply.github.com>
2024-06-11 14:07:40 -0500

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -25,6 +25,25 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    int sepiaRed;
+    int sepiaGreen;
+    int sepiaBlue;
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Compute sepia values
+            sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
+            sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
+            sepiaBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+
+            // Update pixel with sepia values
+            image[i][j].rgbtBlue = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtRed = sepiaBlue;
+        }
+    }
     return;
 }
 
